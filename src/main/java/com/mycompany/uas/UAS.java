84dcafbfff3870f97195a50267116e91819c6139
Niccc7 <162395022+Niccc7@users.noreply.github.com>
2025-12-25 16:31:02 +0700

    produk

---
+++ src/main/java/com/mycompany/uas/UAS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.uas;
+
+/**
+ *
+ * @author NICHOLAS
+ */
+public class UAS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
